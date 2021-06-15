51a01071985a534c30f6bf51f2c520075c689b30
negabaro <negabaro@naver.com>
2021-06-15 21:50:53 +0900

    fix2

--- slack.py
+++ slack.py
@@ -5,8 +5,8 @@ import requests
 from os import getenv
 
 # --- Slack Setting ---
-SLACK_WEBHOOK='https://hooks.slack.com/services/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'
-#SLACK_WEBHOOK=getenv('SLACK_WEBHOOK')
+
+SLACK_WEBHOOK=getenv('SLACK_WEBHOOK')
 SLACK_CHANNEL='#hoiku-watch'
 SLACK_USER='一時保育空き警報発令！！'
 SLACK_TEXT="空きがでたっぽいよ！！急げ"
