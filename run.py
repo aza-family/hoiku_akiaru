e52c81e49ce84347827e42728ddaf6ede2369534
kim <negabaro@naver.com>
2021-12-08 00:30:15 +0900

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from slack import Slack
 
 #url = 'https://koto-kosodate-portal.jp/smf/mizube/general/refresh_cal.php?center_cd=20' #大島
-url = 'https://koto-kosodate-portal.jp/smf/mizube/general/refresh_cal.php?center_cd=10'
+url = 'https://koto-kosodate-portal.jp/smf/mizube/general/refresh_cal.php?center_cd=10' #東陽
 res = requests.get(url)
 
 soup = BeautifulSoup(res.text, 'html.parser')
