d62d95b36ad84b7f9d6849688b3eaa9ae1b5578c
negabaro <negabaro@naver.com>
2022-01-06 10:14:16 +0900

    update

--- run.py
+++ run.py
@@ -13,9 +13,11 @@ soup = BeautifulSoup(res.text, 'html.parser')
 # print('soup:',soup)
 
 #color:#000000;background-color:#87ceeb;border:solid 1px;
-result = soup.find_all('p', attrs={'style':'color:#000000;background-color:#87ceeb;border:solid 1px;'})
+#print(soup.find_all)
+result = soup.find_all('a', attrs={'class':'bgG'})
+#result = soup.find_all('div', attrs={'class':'full'})
 #result = soup.find_all('p', attrs={'style':'margin: 0 5px 5px 0;'})
-result2 = soup.find_all('p', attrs={'style':'color:#000000;background-color:#00bfff;border:solid 1px;'})
+#result2 = soup.find_all('p', attrs={'style':'color:#000000;background-color:#00bfff;border:solid 1px;'})
 
 if result:
     print('result:',result)
@@ -24,11 +26,11 @@ else:
    print('result is empty:',result)
    #Slack.post()
 
-if result2:
-    print('result:',result2)
-    Slack.post()
-else:
-   print('result is empty:',result2)
+# if result2:
+#     print('result2:',result2)
+#     Slack.post()
+# else:
+#    print('result2 is empty:',result2)
    #Slack.post()
 # --- Slack Post ---
 
