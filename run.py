3cee58628be04f81a12faf48815b9943322c0170
negabaro <negabaro@naver.com>
2022-01-30 17:28:32 +0900

    大島追加

--- run.py
+++ run.py
@@ -4,17 +4,12 @@ import requests
 from bs4 import BeautifulSoup
 from slack import Slack
 
-# url = 'https://koto-kosodate-portal.jp/smf/mizube/general/refresh_cal.php?center_cd=20' #大島
 url = 'https://koto-kosodate-portal.jp/smf/mizube/general/refresh_cal.php?center_cd=10' #東陽
+url2 = 'https://koto-kosodate-portal.jp/smf/mizube/general/refresh_cal.php?center_cd=20' #大島
 # url = 'https://koto-kosodate-portal.jp/smf/mizube/general/refresh_cal.php?center_cd=40' #南砂
 res = requests.get(url)
 
 soup = BeautifulSoup(res.text, 'html.parser')
-# print('soup:',soup)
-
-#color:#000000;background-color:#87ceeb;border:solid 1px;
-#print(soup.find_all)
-
 result = soup.find_all('a', attrs={ 'class': lambda val: val in ['bgG', 'bgB'] }) 
 #result = soup.find_all('a', attrs={'class':'bgG'})
 #result = soup.find_all('div', attrs={'class':'full'})
@@ -22,8 +17,8 @@ result = soup.find_all('a', attrs={ 'class': lambda val: val in ['bgG', 'bgB'] }
 #result2 = soup.find_all('p', attrs={'style':'color:#000000;background-color:#00bfff;border:solid 1px;'})
 
 if result:
-    print('result:',result)
-    Slack.post()
+   print('result:',result)
+   Slack.post(10)
 else:
    print('result is empty:',result)
    #Slack.post()
@@ -36,3 +31,16 @@ else:
    #Slack.post()
 # --- Slack Post ---
 
+# ---------------------------
+
+res2 = requests.get(url2)
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+
+result2 = soup2.find_all('a', attrs={ 'class': lambda val: val in ['bgG', 'bgB'] })
+
+if result2:
+   print('result２:',result2)
+   Slack(20).post()
+else:
+   print('result2 is empty:',result2)
+   # Slack(20).post()
